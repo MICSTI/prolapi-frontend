b8b198c559ee4d02d020903f4fbb65a1ec9340a2
Michael Stifter <stifter@parkside.at>
2019-10-06 10:28:07 +0200

    fix: remove interval$ from initial stage

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <section>
     <header class="is-size-3 has-text-centered">🚀 Vue.js and RxJS are awesome! 🚀</header>
 
-    <div class="is-size-4 has-text-centered has-text-weight-bold">{{interval$}}</div>
+    <div class="is-size-4 has-text-centered has-text-weight-bold"></div>
   </section>
 </template>
 
