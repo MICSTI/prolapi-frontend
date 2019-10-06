e31a7b1938a8b956cabf239bad304852072dc7f4
Michael Stifter <stifter@parkside.at>
2019-10-06 11:16:31 +0200

    feat: set stage for http lesson

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,20 @@
     <section class="section">
       <button class="button" v-stream:click="click$">Click</button>
       <div class="title">{{random$}}</div>
-      <div class="title">{{interval$}}</div>
-      <div class="title">{{squared$}}</div>
     </section>
   </section>
 </template>
 
 <script>
-import { interval } from "rxjs";
-import { map, takeWhile, endWith } from "rxjs/operators";
+import { map } from "rxjs/operators";
 
 export default {
   domStreams: ["click$"],
   subscriptions() {
-    const interval$ = interval(1000).pipe(
-      takeWhile(x => x < 6),
-      endWith(100)
-    );
-
-    const squared$ = interval$.pipe(map(x => x * x));
-
     const random$ = this.click$.pipe(map(() => Math.random()));
 
     return {
-      interval$,
-      random$,
-      squared$
+      random$
     };
   }
 };
