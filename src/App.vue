c8672153cae5c3af820858bada2a8111409ddd29
Michael Stifter <stifter@parkside.at>
2019-10-06 17:05:46 +0200

    feat: implement error handling solution

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <header class="is-size-3 has-text-centered">🚀 Vue.js and RxJS are awesome! 🚀</header>
 
     <section class="section">
-      <button :disabled="disabled$" class="button" v-stream:click="click$">{{loadText$}}</button>
+      <button class="button" v-stream:click="click$">Load</button>
+      <div v-if="error$">
+        <span class="tag is-size-4 is-danger">You appear offline 🙀</span>
+        <button class="button" v-stream:click="retry$">Retry</button>
+      </div>
       <div class="title">{{name$}}</div>
       <div>
         <img :src="logo$" alt>
@@ -13,7 +17,8 @@
 </template>
 
 <script>
-import { from, merge } from "rxjs";
+import { from, merge, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
 import {
   map,
   pluck,
@@ -21,19 +26,28 @@ import {
   switchMap,
   mapTo,
   share,
-  startWith
+  startWith,
+  tap,
+  catchError,
+  retryWhen,
+  delay
 } from "rxjs/operators";
 
 export default {
-  domStreams: ["click$"],
+  domStreams: ["click$", "retry$"],
   subscriptions() {
-    const loadUrl = url => from(this.$http.get(url)).pipe(pluck("data"));
-
-    const js$ = this.click$.pipe(
+    const js$ = merge(this.click$, this.retry$).pipe(
       mapTo("http://localhost:3333/api/languages/1"),
-      exhaustMap(loadUrl),
+      switchMap(url =>
+        ajax(url).pipe(
+          pluck("response"),
+          retryWhen(err => err.pipe(delay(3000)))
+        )
+      ),
       share()
     );
+
+    const error$ = js$.pipe(map(value => value === "error"));
 
     const name$ = js$.pipe(pluck("name"));
     const logo$ = js$.pipe(
@@ -41,20 +55,10 @@ export default {
       map(file => `http://localhost:3333/public/${file}`)
     );
 
-    const disabled$ = merge(
-      this.click$.pipe(mapTo(true)),
-      js$.pipe(mapTo(false))
-    ).pipe(startWith(false));
-
-    const loadText$ = disabled$.pipe(
-      map(disabled => (disabled ? "Loading..." : "Load"))
-    );
-
     return {
       name$,
       logo$,
-      disabled$,
-      loadText$
+      error$
     };
   }
 };
